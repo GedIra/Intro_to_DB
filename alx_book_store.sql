a57f486d0bd3b7f56928e29a843719f7e37ff7c6
GedIra <irankundag65@gmail.com>
2024-10-20 18:53:35 +0200

    creation of alx_book_store.sdl file

---
+++ alx_book_store.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE  IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Authors(
+  author_id INT PRIMARY KEY,
+  author_name VARCHAR(215)
+);
+
+CREATE TABLE Book(
+  book_id INT PRIMARY KEY,
+  title VARCHAR(130),
+  author_id  INT FOREIGN KEY REFERENCES author(author_id),
+  price DOUBLE,
+  publication_date DATE
+);
+
+CREATE TABLE Customers(
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT
+);
+
+CREATE TABLE Orders(
+  order_id INT PRIMARY KEY,
+  customer_id INT FOREIGN KEY REFERENCES Customers(customer_id),
+  order_date DATE
+);
+
+CREATE TABLE Order_Details(
+  orderdetailid INT PRIMARY KEY,
+  order_id INT FOREIGN KEY REFERENCES Orders(order_id),
+  book_id INT FOREIGN KEY REFERENCES Books(book_id),
+  quantity DOUBLE
+);
+
+
+
+
